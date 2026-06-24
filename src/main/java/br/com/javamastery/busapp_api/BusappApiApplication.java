102f1394ef0f89adf71aeb859d7752e28a0efe9f
Emanoel <emanoel.pluto@gmail.com>
2026-06-24 09:24:13 -0300

    creating busapp-api repository

---
+++ src/main/java/br/com/javamastery/busapp_api/BusappApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.javamastery.busapp_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusappApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusappApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/javamastery/busapp_api/BusappApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.javamastery.busapp_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusappApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
