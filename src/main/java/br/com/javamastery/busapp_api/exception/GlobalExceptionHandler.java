087d4a9dffd3e2dde6a0a877ed6319c4d0e0886e
Emanoel <emanoel.pluto@gmail.com>
2026-06-27 14:34:58 -0300

    finishing building GlobalExceptionHandlerException class

--- src/main/java/br/com/javamastery/busapp_api/exception/GlobalExceptionHandler.java
+++ src/main/java/br/com/javamastery/busapp_api/exception/GlobalExceptionHandler.java
@@ -1,7 +1,53 @@
 package br.com.javamastery.busapp_api.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(HandlerConfig.class)
+    public ResponseEntity<Object> customHandlerException(HandlerConfig ex){
+        Map<String,Object> map = new HashMap<>();
+
+        map.put("TimeStamp", LocalDateTime.now());
+        map.put("Message",ex.getMessage());
+        map.put("Error","REVIEW THE DATA!" );
+        map.put("Status",ex.getStatus().value());
+
+        return ResponseEntity.status(ex.getStatus()).body(map);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, Object>> handleValidation(MethodArgumentNotValidException ex){
+        String message = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(e -> e.getField() + ":" + e.getDefaultMessage())
+                .findFirst()
+                .orElse("Validation Failed");
+
+        return buildResponse(new HandlerConfig(HttpStatus.BAD_REQUEST, message));
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Map<String, Object>> handleIllegalArgument(IllegalArgumentException ex){
+        return buildResponse(new HandlerConfig(HttpStatus.BAD_REQUEST, ex.getMessage()));
+    }
+
+    private ResponseEntity<Map<String,Object>> buildResponse(HandlerConfig ex){
+        Map<String,Object> map = new HashMap<>();
+        map.put("TimeStamp", LocalDateTime.now().toString());
+        map.put("Status",ex.getStatus().value());
+        map.put("Message",ex.getMessage());
+
+        return ResponseEntity.status(ex.getStatus()).body(map);
+    }
 }
