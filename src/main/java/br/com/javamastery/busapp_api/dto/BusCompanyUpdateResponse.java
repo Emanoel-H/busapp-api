c650676fcd12784aab9c092a6d0d01717081b167
Emanoel <emanoel.pluto@gmail.com>
2026-06-27 10:58:06 -0300

    adding Builder annotation to BusCompanyUpdateResponse class

--- src/main/java/br/com/javamastery/busapp_api/dto/BusCompanyUpdateResponse.java
+++ src/main/java/br/com/javamastery/busapp_api/dto/BusCompanyUpdateResponse.java
@@ -1,10 +1,12 @@
 package br.com.javamastery.busapp_api.dto;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 public class BusCompanyUpdateResponse {
     private Long id;
     private String legalName;
