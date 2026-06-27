56c08def4294de51cad841d527230040a74897ab
Emanoel <emanoel.pluto@gmail.com>
2026-06-27 10:57:18 -0300

    building BusCompanyUpdateResponse class

--- src/main/java/br/com/javamastery/busapp_api/dto/BusCompanyUpdateResponse.java
+++ src/main/java/br/com/javamastery/busapp_api/dto/BusCompanyUpdateResponse.java
@@ -1,4 +1,14 @@
 package br.com.javamastery.busapp_api.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class BusCompanyUpdateResponse {
+    private Long id;
+    private String legalName;
+    private String tradingName;
+    private String cnpj;
+    private String telephone;
 }
