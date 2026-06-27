0d8957a6f59a8fa3ec6440adb0046acd3bad6dd6
Emanoel <emanoel.pluto@gmail.com>
2026-06-27 08:32:57 -0300

    building BusCompanyRepository interface

--- src/main/java/br/com/javamastery/busapp_api/repository/BusCompanyRepository.java
+++ src/main/java/br/com/javamastery/busapp_api/repository/BusCompanyRepository.java
@@ -3,6 +3,12 @@ package br.com.javamastery.busapp_api.repository;
 import br.com.javamastery.busapp_api.model.BusCompany;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BusCompanyRepository extends JpaRepository<BusCompany, Long> {
-    
+    Optional<BusCompany> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+
+    boolean existsByCnpj(String cnpj);
 }
