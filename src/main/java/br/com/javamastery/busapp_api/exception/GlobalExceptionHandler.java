93ec2213e3f024f5360affe97863dbccc2698a88
Emanoel <emanoel.pluto@gmail.com>
2026-06-27 14:11:01 -0300

    creating GlobalExceptionHandler class

---
+++ src/main/java/br/com/javamastery/busapp_api/exception/GlobalExceptionHandler.java
@@ -0,0 +1,7 @@
+package br.com.javamastery.busapp_api.exception;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+}
