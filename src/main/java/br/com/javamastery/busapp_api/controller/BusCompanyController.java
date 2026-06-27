10f16de98001da699b75e5270d3bdb974efacbbe
Emanoel <emanoel.pluto@gmail.com>
2026-06-27 11:32:23 -0300

    creating register() at BusCompanyController

--- src/main/java/br/com/javamastery/busapp_api/controller/BusCompanyController.java
+++ src/main/java/br/com/javamastery/busapp_api/controller/BusCompanyController.java
@@ -1,5 +1,25 @@
 package br.com.javamastery.busapp_api.controller;
 
-public class BusCompanyController {
+import br.com.javamastery.busapp_api.dto.BusCompanyRequest;
+import br.com.javamastery.busapp_api.dto.BusCompanyResponse;
+import br.com.javamastery.busapp_api.service.BusCompanyService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/companies")
+@RequiredArgsConstructor
+public class BusCompanyController {
+    private final BusCompanyService service;
+
+    @PostMapping
+    public ResponseEntity<BusCompanyResponse> register(@RequestBody @Valid BusCompanyRequest busCompanyRequest) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.register(busCompanyRequest));
+    }
 }
