737fdad570bc0ce9ca8b1825648eae6e13cd8f9e
Emanoel <emanoel.pluto@gmail.com>
2026-06-27 11:37:17 -0300

    creating findById() at BusCompanyController

--- src/main/java/br/com/javamastery/busapp_api/controller/BusCompanyController.java
+++ src/main/java/br/com/javamastery/busapp_api/controller/BusCompanyController.java
@@ -7,10 +7,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/companies")
@@ -21,5 +18,10 @@ public class BusCompanyController {
     @PostMapping
     public ResponseEntity<BusCompanyResponse> register(@RequestBody @Valid BusCompanyRequest busCompanyRequest) {
         return ResponseEntity.status(HttpStatus.CREATED).body(service.register(busCompanyRequest));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<BusCompanyResponse> getById(@PathVariable Long id) {
+        return ResponseEntity.ok(service.findById(id));
     }
 }
