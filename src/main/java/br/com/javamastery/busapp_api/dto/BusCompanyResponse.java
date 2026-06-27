f85614a8e6b11e472a9d16898494aabff2647035
Emanoel <emanoel.pluto@gmail.com>
2026-06-27 08:56:33 -0300

    building BusCompanyResponse class

--- src/main/java/br/com/javamastery/busapp_api/dto/BusCompanyResponse.java
+++ src/main/java/br/com/javamastery/busapp_api/dto/BusCompanyResponse.java
@@ -3,7 +3,16 @@ package br.com.javamastery.busapp_api.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 public class BusCompanyResponse {
+    private Long id;
+    private String legalName;
+    private String tradingName;
+    private String cnpj;
+    private String telephone;
+    private String email;
+    private LocalDateTime createdAt;
 }
