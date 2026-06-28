49cbda240a910b466d115c38fa6745ce7e92f786
Emanoel <emanoel.pluto@gmail.com>
2026-06-28 07:49:30 -0300

    building TravelerRequest class

--- src/main/java/br/com/javamastery/busapp_api/dto/TravelerRequest.java
+++ src/main/java/br/com/javamastery/busapp_api/dto/TravelerRequest.java
@@ -1,4 +1,31 @@
 package br.com.javamastery.busapp_api.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
 public class TravelerRequest {
+    @NotBlank(message = "Name is required!")
+    private String name;
+    @NotBlank(message = "Birth Date is required!")
+    private LocalDate birthDate;
+    @NotBlank(message = "CPF is required!")
+    @Pattern(regexp = "\\d{11}", message = "CPF must contain 11 digits!")
+    private String cpf;
+    @Pattern(regexp = "\\d{10,11}", message = "Telephone must contain 10 or 11 digits!")
+    @NotBlank(message = "Telephone is required!")
+    private String telephone;
+    @NotBlank(message = "Email is required!")
+    @Email(message = "Invalid email format")
+    private String email;
+    @NotBlank(message = "Password is required!")
+    @Size(min = 6, max = 16, message = "Password must be between 6 and 16 characters")
+    private String password;
 }
