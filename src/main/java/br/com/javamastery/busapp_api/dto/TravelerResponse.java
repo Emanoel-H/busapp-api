491e8607d16088fbbd7ff63daac03b89ed84d5c9
Emanoel <emanoel.pluto@gmail.com>
2026-06-28 07:53:52 -0300

    building TravelerResponse class

--- src/main/java/br/com/javamastery/busapp_api/dto/TravelerResponse.java
+++ src/main/java/br/com/javamastery/busapp_api/dto/TravelerResponse.java
@@ -1,4 +1,22 @@
 package br.com.javamastery.busapp_api.dto;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
 public class TravelerResponse {
+    private Long id;
+    private String name;
+    private String cpf;
+    private LocalDate birthDate;
+    private int age;
+    private String email;
+    private String telephone;
+    private LocalDateTime createdAt;
 }
