d0f94c03bc280e7490c59e3d7ed841c2f74177c2
Emanoel <emanoel.pluto@gmail.com>
2026-06-28 07:35:59 -0300

    building ValidCpf annotation class

--- src/main/java/br/com/javamastery/busapp_api/validation/ValidCpf.java
+++ src/main/java/br/com/javamastery/busapp_api/validation/ValidCpf.java
@@ -1,4 +1,16 @@
 package br.com.javamastery.busapp_api.validation;
 
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = CpfValidator.class)
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface ValidCpf {
+    String message() default "invalid CPF";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
 }
