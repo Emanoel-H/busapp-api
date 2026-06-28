1d2fa7420c0ad2ecca827acc613a4156fc324ffd
Emanoel <emanoel.pluto@gmail.com>
2026-06-28 08:02:15 -0300

    building TravelerUpdateRequest class

--- src/main/java/br/com/javamastery/busapp_api/dto/TravelerUpdateRequest.java
+++ src/main/java/br/com/javamastery/busapp_api/dto/TravelerUpdateRequest.java
@@ -1,4 +1,24 @@
 package br.com.javamastery.busapp_api.dto;
 
+import br.com.javamastery.busapp_api.validation.ValidCpf;
+import jakarta.validation.constraints.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
 public class TravelerUpdateRequest {
+    @NotBlank(message = "Name is required!")
+    private String name;
+    @NotBlank(message = "Birth Date is required!")
+    @Past(message = "Birth Date must be in the past")
+    private LocalDate birthDate;
+    @NotBlank(message = "CPF is required!")
+    @ValidCpf
+    private String cpf;
+    @Pattern(regexp = "\\d{10,11}", message = "Telephone must contain 10 or 11 digits!")
+    @NotBlank(message = "Telephone is required!")
+    private String telephone;
 }
