2ccdf13a95b5c133186a044ecc49ec731f1618f9
Emanoel <emanoel.pluto@gmail.com>
2026-06-29 11:06:26 -0300

    building StateRepository interface

--- src/main/java/br/com/javamastery/busapp_api/repository/StateRepository.java
+++ src/main/java/br/com/javamastery/busapp_api/repository/StateRepository.java
@@ -1,4 +1,11 @@
 package br.com.javamastery.busapp_api.repository;
 
-public interface StateRepository {
+import br.com.javamastery.busapp_api.model.State;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface StateRepository extends JpaRepository<State,Long> {
+    Optional<State> findByUf(String uf);
+    Optional<State> findByNameIgnoreCase(String name);
 }
