09f5e7c29980ab35266917a1e315cc2042e7be46
Emanoel <emanoel.pluto@gmail.com>
2026-06-29 10:25:26 -0300

    building State class

--- src/main/java/br/com/javamastery/busapp_api/model/State.java
+++ src/main/java/br/com/javamastery/busapp_api/model/State.java
@@ -1,4 +1,19 @@
 package br.com.javamastery.busapp_api.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+
+@Entity
+@Table(name = "states")
+@Getter
 public class State {
+    @Id
+    @Column(name = "state_code")
+    Long state_code;
+    String uf;
+    String name;
+    String region;
 }
