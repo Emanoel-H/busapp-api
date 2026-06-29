e953d377d4f50a76a8ed8d188ef5d28fac5675c1
Emanoel <emanoel.pluto@gmail.com>
2026-06-29 07:13:23 -0300

    fixing findByEmail() at TravelerService

--- src/main/java/br/com/javamastery/busapp_api/repository/TravelerRepository.java
+++ src/main/java/br/com/javamastery/busapp_api/repository/TravelerRepository.java
@@ -1,13 +1,12 @@
 package br.com.javamastery.busapp_api.repository;
 
-import br.com.javamastery.busapp_api.model.BusCompany;
 import br.com.javamastery.busapp_api.model.Traveler;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface TravelerRepository extends JpaRepository<Traveler, Long> {
-    Optional<BusCompany> findByEmail(String email);
+    Optional<Traveler> findByEmail(String email);
 
     boolean existsByEmail(String email);
 
