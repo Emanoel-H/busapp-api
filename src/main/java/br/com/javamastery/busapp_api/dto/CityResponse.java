ddcc4214d5f0962d39826f4c43fe95fdfaf4c4ab
Emanoel <emanoel.pluto@gmail.com>
2026-06-29 11:25:13 -0300

    building CityResponse class

--- src/main/java/br/com/javamastery/busapp_api/dto/CityResponse.java
+++ src/main/java/br/com/javamastery/busapp_api/dto/CityResponse.java
@@ -1,4 +1,14 @@
 package br.com.javamastery.busapp_api.dto;
 
+import br.com.javamastery.busapp_api.model.State;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
 public class CityResponse {
+    private Long ibgeCode;
+    private String city;
+    private State state;
+    private int ddd;
 }
