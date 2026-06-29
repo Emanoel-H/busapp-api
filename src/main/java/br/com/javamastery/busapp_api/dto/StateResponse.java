57ea150adea3f8cb312a810c9bb29dcdda7d60b9
Emanoel <emanoel.pluto@gmail.com>
2026-06-29 11:26:39 -0300

    building StateResponse class

--- src/main/java/br/com/javamastery/busapp_api/dto/StateResponse.java
+++ src/main/java/br/com/javamastery/busapp_api/dto/StateResponse.java
@@ -1,4 +1,13 @@
 package br.com.javamastery.busapp_api.dto;
 
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
 public class StateResponse {
+    private Long code;
+    private String uf;
+    private String name;
+    private String region;
 }
