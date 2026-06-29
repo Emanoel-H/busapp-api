78322babe0e287a65a6fb76941e7c50091913e61
Emanoel <emanoel.pluto@gmail.com>
2026-06-29 10:30:27 -0300

    building City class

--- src/main/java/br/com/javamastery/busapp_api/model/City.java
+++ src/main/java/br/com/javamastery/busapp_api/model/City.java
@@ -1,4 +1,21 @@
 package br.com.javamastery.busapp_api.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Entity
+@Table(name = "cities")
+@Getter
 public class City {
+    @Id
+    @Column(name = "ibge_code")
+    Long ibgeCode;
+    String city;
+    double latitude;
+    double longitude;
+    int ddd;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "state_code")
+    State state;
+
 }
