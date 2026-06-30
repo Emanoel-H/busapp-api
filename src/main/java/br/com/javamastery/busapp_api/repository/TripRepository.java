67660f58e6affd06fbca9537d74c16aded4b0f83
Emanoel <emanoel.pluto@gmail.com>
2026-06-30 16:01:44 -0300

    building TripRepository interface

--- src/main/java/br/com/javamastery/busapp_api/repository/TripRepository.java
+++ src/main/java/br/com/javamastery/busapp_api/repository/TripRepository.java
@@ -1,4 +1,26 @@
 package br.com.javamastery.busapp_api.repository;
 
-public interface TripRepository {
+import br.com.javamastery.busapp_api.model.City;
+import br.com.javamastery.busapp_api.model.Trip;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface TripRepository extends JpaRepository<Trip, Long> {
+    @Query("SELECT t FROM Trip t JOIN FETCH t.origin oc JOIN FETCH oc.state JOIN FETCH t.destination dc JOIN FETCH dc.state JOIN FETCH t.busCompany bc WHERE t.code = :code")
+    Optional<Trip> findByCode(@Param("code") String code);
+
+    @Query("SELECT t FROM Trip t JOIN FETCH t.origin oc JOIN FETCH oc.state JOIN FETCH t.destination dc JOIN FETCH dc.state JOIN FETCH t.busCompany bc WHERE t.active = true")
+    List<Trip> findAllActive();
+
+    boolean existsByCode(String code);
+
+    @Query("SELECT t FROM Trip t JOIN FETCH t.origin oc JOIN FETCH oc.state JOIN FETCH t.destination dc JOIN FETCH dc.state JOIN FETCH t.busCompany bc WHERE bc.id = :busCompanyId AND t.active = true")
+    List<Trip> findAllByBusCompanyId(@Param("busCompanyId") Long busCompanyId);
+
+    @Query("SELECT t FROM Trip t JOIN FETCH t.origin oc JOIN FETCH oc.state JOIN FETCH t.destination dc JOIN FETCH dc.state JOIN FETCH t.busCompany bc WHERE oc.ibgeCode = :originCode AND dc.ibgeCode = :destinationCode AND t.active = true")
+    List<Trip> findAllByOriginAndDestination(@Param("originCode") Long originCode, @Param("destinationCode") Long destinationCode);
 }
