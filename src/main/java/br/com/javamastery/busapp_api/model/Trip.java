85086931be7d5612587a579e317fe6f4e6508096
Emanoel <emanoel.pluto@gmail.com>
2026-06-30 11:30:04 -0300

    building Trip class

--- src/main/java/br/com/javamastery/busapp_api/model/Trip.java
+++ src/main/java/br/com/javamastery/busapp_api/model/Trip.java
@@ -1,4 +1,79 @@
 package br.com.javamastery.busapp_api.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+@Entity
+@Table(name = "trips")
+@Getter
+@Setter
 public class Trip {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(length = 10,unique = true, nullable = false)
+    private String code;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "origin_city_id",  nullable = false)
+    private City origin;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "destination_city_id",  nullable = false)
+    private City destination;
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal price;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "company_id", nullable = false)
+    private BusCompany busCompany;
+    @Column(nullable = false)
+    private LocalTime departureTime;
+    private boolean active = true;
+    @Column(nullable = false)
+    private double distanceKM;
+    @Enumerated(EnumType.STRING)
+    private Category category;
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+        generateCode();
+        calculateCategory();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = LocalDateTime.now();
+        calculateCategory();
+    }
+
+    private void generateCode(){
+        if(this.code == null){
+            String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            StringBuilder stringBuilder = new StringBuilder();
+            java.util.Random random = new java.util.Random();
+
+            while(stringBuilder.length() < 10)
+                stringBuilder.append(chars.charAt(random.nextInt(chars.length())));
+
+            this.code = stringBuilder.toString();
+
+        }
+    }
+
+    private void calculateCategory(){
+        if (this.origin != null && this.destination != null) {
+                this.category = this.origin.getState().getUf().equals(this.destination.getState().getUf())
+                        ? Category.INTERCITY
+                        : Category.INTERSTATE;
+        }
+    }
+
 }
