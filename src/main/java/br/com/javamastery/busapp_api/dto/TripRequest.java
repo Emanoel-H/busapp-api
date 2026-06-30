3880cfcc27218e4d155fa318cffa45a8f0ee1ceb
Emanoel <emanoel.pluto@gmail.com>
2026-06-30 11:38:46 -0300

    building TripRequest class

--- src/main/java/br/com/javamastery/busapp_api/dto/TripRequest.java
+++ src/main/java/br/com/javamastery/busapp_api/dto/TripRequest.java
@@ -1,4 +1,25 @@
 package br.com.javamastery.busapp_api.dto;
 
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalTime;
+
+@Getter
+@Setter
 public class TripRequest {
+    @NotNull(message = "Origin City IBGE code is required!")
+    private Long originCityIbgeCode;
+    @NotNull(message = "Destination City IBGE code is required!")
+    private Long destinationCityIbgeCode;
+    @NotNull(message = "Company ID is required!")
+    private Long busCompanyId;
+    @NotNull(message = "Price is required")
+    @DecimalMin(value = "0.01", message = "Price must be greater than zero!")
+    private BigDecimal price;
+    @NotNull(message = "Departure time is required! (Use 24-hour format, example: 14:30)")
+    private LocalTime departureTime;
 }
