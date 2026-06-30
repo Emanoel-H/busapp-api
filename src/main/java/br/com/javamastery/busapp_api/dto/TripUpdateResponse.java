ee97a05e8f5389a348937005e07b2a059d866539
Emanoel <emanoel.pluto@gmail.com>
2026-06-30 16:12:51 -0300

    building TripUpdateResponse class

--- src/main/java/br/com/javamastery/busapp_api/dto/TripUpdateResponse.java
+++ src/main/java/br/com/javamastery/busapp_api/dto/TripUpdateResponse.java
@@ -1,4 +1,24 @@
 package br.com.javamastery.busapp_api.dto;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalTime;
+
+@Getter
+@Setter
+@Builder
 public class TripUpdateResponse {
+    private Long id;
+    private String code;
+    private String originCity;
+    private String originState;
+    private String destinationCity;
+    private String destinationState;
+    private BigDecimal price;
+    private LocalTime departureTime;
+    private double distanceKM;
+    private String category;
 }
