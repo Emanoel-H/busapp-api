0a6cc9299924abc2fe5b9bca899511694b20355e
Emanoel <emanoel.pluto@gmail.com>
2026-06-30 21:31:14 -0300

    building TripService class

--- src/main/java/br/com/javamastery/busapp_api/service/TripService.java
+++ src/main/java/br/com/javamastery/busapp_api/service/TripService.java
@@ -1,4 +1,130 @@
 package br.com.javamastery.busapp_api.service;
 
+import br.com.javamastery.busapp_api.dto.TripRequest;
+import br.com.javamastery.busapp_api.dto.TripResponse;
+import br.com.javamastery.busapp_api.dto.TripUpdateRequest;
+import br.com.javamastery.busapp_api.dto.TripUpdateResponse;
+import br.com.javamastery.busapp_api.exception.HandlerConfig;
+import br.com.javamastery.busapp_api.model.BusCompany;
+import br.com.javamastery.busapp_api.model.City;
+import br.com.javamastery.busapp_api.model.Trip;
+import br.com.javamastery.busapp_api.repository.BusCompanyRepository;
+import br.com.javamastery.busapp_api.repository.CityRepository;
+import br.com.javamastery.busapp_api.repository.TripRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class TripService {
+    private final TripRepository tripRepository;
+    private final BusCompanyRepository busCompanyRepository;
+    private final CityRepository cityRepository;
+
+    public TripResponse create(TripRequest tripRequest) {
+        City origin = findCityOrThrow(tripRequest.getOriginCityIbgeCode());
+        City destination = findCityOrThrow(tripRequest.getDestinationCityIbgeCode());
+        BusCompany busCompany = findBusCompanyOrThrow(tripRequest.getBusCompanyId());
+
+        return toResponse(tripRepository.save(new Trip(tripRequest, origin, destination, busCompany)));
+    }
+
+    public City findCityOrThrow (Long ibgeCode) {
+        return cityRepository.findByIBGECode(ibgeCode)
+                .orElseThrow(() -> new HandlerConfig(HttpStatus.NOT_FOUND, "City not found for the code: " + ibgeCode));
+    }
+
+    public BusCompany findBusCompanyOrThrow (Long id) {
+        return busCompanyRepository.findById(id)
+                .orElseThrow(() -> new HandlerConfig(HttpStatus.NOT_FOUND, "Company not found for the id: " + id));
+    }
+
+    public Trip findTripOrThrow (Long id) {
+        return tripRepository.findById(id)
+                .orElseThrow(() -> new HandlerConfig(HttpStatus.NOT_FOUND, "Trip not found for the id: " + id));
+    }
+
+    public Trip findTripOrThrow(String code){
+        return tripRepository.findByCode(code)
+                .orElseThrow(() -> new HandlerConfig(HttpStatus.NOT_FOUND, "Trip not found for the code: " + code));
+    }
+
+    public List<TripResponse> listByRoute(Long originCode, Long destinationCode) {
+        List<Trip> trips = tripRepository.findAllByOriginAndDestination(originCode, destinationCode);
+
+        if (trips.isEmpty())
+            throw new HandlerConfig(HttpStatus.NOT_FOUND, "No trips found for this route");
+
+        return trips.stream().map(this::toResponse).toList();
+    }
+
+    public List<TripResponse> listAll(){
+        List<Trip> trips = tripRepository.findAllActive();
+
+        if (trips.isEmpty())
+            throw new HandlerConfig(HttpStatus.NOT_FOUND, "There are no active trips");
+
+        return trips.stream().map(this::toResponse).toList();
+    }
+
+    public List<TripResponse> listByCompany(Long companyId) {
+        List<Trip> trips = tripRepository.findAllByBusCompanyId(companyId);
+
+        if (trips.isEmpty())
+            throw new HandlerConfig(HttpStatus.NOT_FOUND, "No trips found for the company with id: " + companyId);
+
+        return trips.stream().map(this::toResponse).toList();
+    }
+
+    public TripResponse findByCode(String code) {
+        return toResponse(findTripOrThrow(code));
+    }
+
+    public TripUpdateResponse update(String code, TripUpdateRequest tripUpdateRequest){
+        Trip trip  = findTripOrThrow(code);
+        City origin = findCityOrThrow(tripUpdateRequest.getOriginCityIbgeCode());
+        City destination = findCityOrThrow(tripUpdateRequest.getDestinationCityIbgeCode());
+        trip.tripUpdateRequest(tripUpdateRequest, origin, destination);
+
+        return toUpdateResponse(tripRepository.save(trip));
+    }
+
+    public void delete(String code){
+        Trip trip = findTripOrThrow(code);
+        trip.deactivate();
+        tripRepository.save(trip);
+    }
+
+    public TripResponse toResponse(Trip trip) {
+        return TripResponse.builder()
+                .code(trip.getCode())
+                .originCity(trip.getOrigin().getCity())
+                .originState(trip.getOrigin().getState().getName())
+                .destinationCity(trip.getDestination().getCity())
+                .destinationState(trip.getDestination().getState().getName())
+                .busCompany(trip.getBusCompany().getTradingName())
+                .price(trip.getPrice())
+                .departureTime(trip.getDepartureTime())
+                .distanceKM(trip.getDistanceKM())
+                .category(trip.getCategory().toString())
+                .build();
+    }
+
+    public TripUpdateResponse toUpdateResponse(Trip trip) {
+        return TripUpdateResponse.builder()
+                .id(trip.getId())
+                .code(trip.getCode())
+                .originCity(trip.getOrigin().getCity())
+                .originState(trip.getOrigin().getState().getName())
+                .destinationCity(trip.getDestination().getCity())
+                .destinationState(trip.getDestination().getState().getName())
+                .price(trip.getPrice())
+                .departureTime(trip.getDepartureTime())
+                .distanceKM(trip.getDistanceKM())
+                .category(trip.getCategory().toString())
+                .build();
+    }
 }
