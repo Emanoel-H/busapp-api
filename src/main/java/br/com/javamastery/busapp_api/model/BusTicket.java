7613304ed41b7db136522d4fa33e0e4c9f9461e3
Emanoel <emanoel.pluto@gmail.com>
2026-07-01 19:01:27 -0300

    adding @Column to tracking field to BusTicket class

--- src/main/java/br/com/javamastery/busapp_api/model/BusTicket.java
+++ src/main/java/br/com/javamastery/busapp_api/model/BusTicket.java
@@ -32,6 +32,7 @@ public class BusTicket {
     private LocalDate saleDate;
     private LocalDateTime cancelDate;
     private boolean canceled = false;
+    @Column(nullable = false, updatable = false)
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 
