0c01e4c9c01404ad1c46356701e956503d3ce9fd
Emanoel <emanoel.pluto@gmail.com>
2026-07-01 19:11:36 -0300

    building BusTicketRequest class

--- src/main/java/br/com/javamastery/busapp_api/dto/BusTicketRequest.java
+++ src/main/java/br/com/javamastery/busapp_api/dto/BusTicketRequest.java
@@ -1,4 +1,19 @@
 package br.com.javamastery.busapp_api.dto;
 
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter
+@Setter
 public class BusTicketRequest {
+    @NotNull(message = "Traveler id is required!")
+    private Long traveler_id;
+    @NotNull(message = "Trip code is required!")
+    private String trip_code;
+    @NotNull(message = "Departure Date is required!")
+    private LocalDate departureDate;
 }
