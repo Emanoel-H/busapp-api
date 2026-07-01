96d2d9e5c0432ab44e9cc8169d3395fc17a6172f
Emanoel <emanoel.pluto@gmail.com>
2026-07-01 19:19:04 -0300

    adding @Future to date field at BusTicketRequest class

--- src/main/java/br/com/javamastery/busapp_api/dto/BusTicketRequest.java
+++ src/main/java/br/com/javamastery/busapp_api/dto/BusTicketRequest.java
@@ -1,5 +1,6 @@
 package br.com.javamastery.busapp_api.dto;
 
+import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
@@ -15,5 +16,6 @@ public class BusTicketRequest {
     @NotNull(message = "Trip code is required!")
     private String trip_code;
     @NotNull(message = "Departure Date is required!")
+    @Future(message = "Departure Date must be in the future!")
     private LocalDate departureDate;
 }
