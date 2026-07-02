b8fd5f2a2ba637c68e6866e1bc6de4752678e64a
Emanoel <emanoel.pluto@gmail.com>
2026-07-02 12:06:48 -0300

    building BusTicketCanceledResponse class

--- src/main/java/br/com/javamastery/busapp_api/dto/BusTicketCanceledResponse.java
+++ src/main/java/br/com/javamastery/busapp_api/dto/BusTicketCanceledResponse.java
@@ -1,4 +1,26 @@
 package br.com.javamastery.busapp_api.dto;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Builder
 public class BusTicketCanceledResponse {
+    private String code;
+    private BigDecimal price;
+    private LocalDate departureDate;
+    private String travelerName;
+    private BigDecimal travelerCreditsBalance;
+    private String cpf;
+    private String originCity;
+    private String originState;
+    private String destinationCity;
+    private String destinationState;
+    private LocalDate cancelDate;
+    private boolean canceled;
 }
