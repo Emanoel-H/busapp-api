c422418a5f435dbdb7b7cd09dccc0993a8f8d57d
Emanoel <emanoel.pluto@gmail.com>
2026-07-02 10:22:09 -0300

    creating findTravelerOrThrow() at BusTicketService class

--- src/main/java/br/com/javamastery/busapp_api/service/BusTicketService.java
+++ src/main/java/br/com/javamastery/busapp_api/service/BusTicketService.java
@@ -1,4 +1,26 @@
 package br.com.javamastery.busapp_api.service;
 
+import br.com.javamastery.busapp_api.dto.BusTicketRequest;
+import br.com.javamastery.busapp_api.dto.BusTicketResponse;
+import br.com.javamastery.busapp_api.exception.HandlerConfig;
+import br.com.javamastery.busapp_api.model.Traveler;
+import br.com.javamastery.busapp_api.repository.BusTicketRepository;
+import br.com.javamastery.busapp_api.repository.TravelerRepository;
+import br.com.javamastery.busapp_api.repository.TripRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class BusTicketService {
+    private final BusTicketRepository busTicketRepository;
+    private final TravelerRepository travelerRepository;
+    private final TripRepository tripRepository;
+
+    public Traveler findTravelerOrThrow(Long traveler_id) {
+        return travelerRepository.findById(traveler_id)
+                .orElseThrow(() -> new HandlerConfig(HttpStatus.NOT_FOUND, "Traveler not found!"));
+    }
 }
