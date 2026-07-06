3c72d65d97768452ee201d86e4f0b3ba30b73de6
Emanoel <emanoel.pluto@gmail.com>
2026-07-06 08:10:47 -0300

    creating travelerToResponse() at AuthService

--- src/main/java/br/com/javamastery/busapp_api/service/AuthService.java
+++ src/main/java/br/com/javamastery/busapp_api/service/AuthService.java
@@ -1,4 +1,25 @@
 package br.com.javamastery.busapp_api.service;
 
+import br.com.javamastery.busapp_api.dto.LoginResponse;
+import br.com.javamastery.busapp_api.model.Traveler;
+import br.com.javamastery.busapp_api.repository.BusCompanyRepository;
+import br.com.javamastery.busapp_api.repository.TravelerRepository;
+import br.com.javamastery.busapp_api.security.JwtService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class AuthService {
+    private final BusCompanyRepository busCompanyRepository;
+    private final TravelerRepository travelerRepository;
+    private final JwtService jwtService;
+
+    public LoginResponse travelerToResponse(Traveler traveler){
+        return LoginResponse.builder()
+                .token(jwtService.generateToken(traveler.getEmail(), "TRAVELER"))
+                .email(traveler.getEmail())
+                .role("TRAVELER")
+                .build();
+    }
 }
