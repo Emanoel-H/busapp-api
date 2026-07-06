3ee2e5732028c46c8a9c5ffa8f39072c88b323d1
Emanoel <emanoel.pluto@gmail.com>
2026-07-06 05:42:23 -0300

    building OsrmRoute class

--- src/main/java/br/com/javamastery/busapp_api/client/dto/OsrmRoute.java
+++ src/main/java/br/com/javamastery/busapp_api/client/dto/OsrmRoute.java
@@ -1,4 +1,13 @@
 package br.com.javamastery.busapp_api.client.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class OsrmRoute {
+    private double distance;
+    private double duration;
 }
