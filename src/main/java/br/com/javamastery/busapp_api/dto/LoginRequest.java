17a38d717030fd17f27ee582101536d4e4b7c12d
Emanoel <emanoel.pluto@gmail.com>
2026-07-06 07:51:53 -0300

    building LoginRequest class

--- src/main/java/br/com/javamastery/busapp_api/dto/LoginRequest.java
+++ src/main/java/br/com/javamastery/busapp_api/dto/LoginRequest.java
@@ -1,4 +1,17 @@
 package br.com.javamastery.busapp_api.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class LoginRequest {
+    @NotBlank(message = "Email is required!")
+    @Email(message = "Invalid email format!")
+    private String email;
+
+    @NotBlank(message = "Password is required!")
+    private String password;
 }
