d3913e912ff064e5a51ea9997d091f9004059d27
Emanoel <emanoel.pluto@gmail.com>
2026-07-06 07:53:35 -0300

    building LoginResponse class

--- src/main/java/br/com/javamastery/busapp_api/dto/LoginResponse.java
+++ src/main/java/br/com/javamastery/busapp_api/dto/LoginResponse.java
@@ -1,4 +1,14 @@
 package br.com.javamastery.busapp_api.dto;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
 public class LoginResponse {
+    private String token;
+    private String email;
+    private String role;
 }
