76d8d121d32c20bbb833ac559f6bf7c191bee9c7
Emanoel <emanoel.pluto@gmail.com>
2026-07-06 09:04:32 -0300

    building AuthController class

--- src/main/java/br/com/javamastery/busapp_api/controller/AuthController.java
+++ src/main/java/br/com/javamastery/busapp_api/controller/AuthController.java
@@ -1,4 +1,21 @@
 package br.com.javamastery.busapp_api.controller;
 
+import br.com.javamastery.busapp_api.dto.LoginRequest;
+import br.com.javamastery.busapp_api.dto.LoginResponse;
+import br.com.javamastery.busapp_api.service.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
 public class AuthController {
+    private final AuthService service;
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@RequestBody @Valid LoginRequest loginRequest) {
+        return ResponseEntity.ok(service.login(loginRequest));
+    }
 }
