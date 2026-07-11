71ebbdbbdbc03a22c6177c6d341dc32d1f3ae512
Emanoel <emanoel.pluto@gmail.com>
2026-07-11 11:36:07 -0300

    creating setUp() at BusTicketServiceTest

--- src/test/java/br/com/javamastery/busapp_api/service/BusTicketServiceTest.java
+++ src/test/java/br/com/javamastery/busapp_api/service/BusTicketServiceTest.java
@@ -1,12 +1,11 @@
 package br.com.javamastery.busapp_api.service;
 
 import br.com.javamastery.busapp_api.exception.HandlerConfig;
-import br.com.javamastery.busapp_api.model.BusTicket;
-import br.com.javamastery.busapp_api.model.Traveler;
-import br.com.javamastery.busapp_api.model.Trip;
+import br.com.javamastery.busapp_api.model.*;
 import br.com.javamastery.busapp_api.repository.BusTicketRepository;
 import br.com.javamastery.busapp_api.repository.TravelerRepository;
 import br.com.javamastery.busapp_api.repository.TripRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -15,6 +14,9 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
@@ -33,6 +35,31 @@ public class BusTicketServiceTest {
     private BusTicket busTicket;
     private Traveler traveler;
     private Trip trip;
+
+    @BeforeEach
+    void setUp(){
+        State state = new  State();
+        City origin = new  City();
+        City destination = new  City();
+
+        trip = new  Trip();
+        trip.setOrigin(origin);
+        trip.setDestination(destination);
+        trip.setDepartureTime(LocalTime.now().plusHours(3));
+        trip.setPrice(BigDecimal.valueOf(120.00));
+        trip.setActive(true);
+
+        traveler = new Traveler();
+        traveler.addCredits(BigDecimal.ZERO);
+
+        busTicket = new BusTicket();
+        busTicket.setCode("TICKET0001");
+        busTicket.setTrip(trip);
+        busTicket.setTraveler(traveler);
+        busTicket.setPrice(BigDecimal.valueOf(120.00));
+        busTicket.setDepartureDate(LocalDate.now().plusDays(1));
+        busTicket.setCanceled(false);
+    }
 
     @Test
     @DisplayName("Should throw NOT_FOUND when ticket code does not exist")
