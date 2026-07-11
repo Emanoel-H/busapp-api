52f079b6515155c7469738286b59734d1df8f52b
Emanoel <emanoel.pluto@gmail.com>
2026-07-11 16:36:25 -0300

    creating emailAlreadyExits_Conflict() at TravelerServiceTest

--- src/test/java/br/com/javamastery/busapp_api/service/TravelerServiceTest.java
+++ src/test/java/br/com/javamastery/busapp_api/service/TravelerServiceTest.java
@@ -1,15 +1,23 @@
 package br.com.javamastery.busapp_api.service;
 
+import br.com.javamastery.busapp_api.exception.HandlerConfig;
 import br.com.javamastery.busapp_api.model.Traveler;
 import br.com.javamastery.busapp_api.repository.TravelerRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowableOfType;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class TravelerServiceTest {
@@ -32,5 +40,15 @@ public class TravelerServiceTest {
         traveler.setCreditsBalance(BigDecimal.valueOf(100.00));
         traveler.setEmail("traveler@gmail.com");
         traveler.setPassword(passwordEncoder.encode("traveler123"));
+    }
+
+    @Test
+    @DisplayName("Should throw CONFLICT when email already exists")
+    void emailAlreadyExits_Conflict(){
+        when(repository.existsByEmail(traveler.getEmail())).thenReturn(true);
+
+        HandlerConfig ex = catchThrowableOfType(() -> repository.existsByEmail(traveler.getEmail()), HandlerConfig.class);
+
+        assertThat(ex.getStatus()).isEqualTo(HttpStatus.CONFLICT);
     }
 }
