1894cd13994aed9fc451dc5b090f604c9dd0b91e
Emanoel <emanoel.pluto@gmail.com>
2026-07-11 10:10:40 -0300

    injecting mock classes at BusTicketServiceTest class

--- src/test/java/br/com/javamastery/busapp_api/service/BusTicketServiceTest.java
+++ src/test/java/br/com/javamastery/busapp_api/service/BusTicketServiceTest.java
@@ -1,4 +1,23 @@
 package br.com.javamastery.busapp_api.service;
 
+import br.com.javamastery.busapp_api.repository.BusTicketRepository;
+import br.com.javamastery.busapp_api.repository.TravelerRepository;
+import br.com.javamastery.busapp_api.repository.TripRepository;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 public class BusTicketServiceTest {
+    @InjectMocks
+    private BusTicketService busTicketService;
+    @Mock
+    private BusTicketRepository busTicketRepository;
+    @Mock
+    private TravelerRepository travelerRepository;
+    @Mock
+    private TripRepository tripRepository;
+
+
 }
