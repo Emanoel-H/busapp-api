070f5d0024c594ea090c908dc5437f9e5420aca3
Emanoel <emanoel.pluto@gmail.com>
2026-07-11 16:28:52 -0300

    creating setUp() at TravelerServiceTest

--- src/test/java/br/com/javamastery/busapp_api/service/TravelerServiceTest.java
+++ src/test/java/br/com/javamastery/busapp_api/service/TravelerServiceTest.java
@@ -1,4 +1,36 @@
 package br.com.javamastery.busapp_api.service;
 
+import br.com.javamastery.busapp_api.model.Traveler;
+import br.com.javamastery.busapp_api.repository.TravelerRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.math.BigDecimal;
+
+@ExtendWith(MockitoExtension.class)
 public class TravelerServiceTest {
+    @InjectMocks
+    private TravelerService service;
+    @Mock
+    private TravelerRepository repository;
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    @Mock
+    private Traveler traveler;
+
+    @BeforeEach
+    void setUp(){
+        traveler = new Traveler();
+        traveler.setId(1L);
+        traveler.setName("Traveler");
+        traveler.setCpf("12345678902");
+        traveler.setCreditsBalance(BigDecimal.valueOf(100.00));
+        traveler.setEmail("traveler@gmail.com");
+        traveler.setPassword(passwordEncoder.encode("traveler123"));
+    }
 }
