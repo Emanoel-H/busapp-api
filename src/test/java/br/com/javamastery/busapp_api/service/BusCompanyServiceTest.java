c45b933e9c63d6004d9894b7dc6d2f677b76c76c
Emanoel <emanoel.pluto@gmail.com>
2026-09-15 10:07:44 -0300

    creating emailAlreadyExits_Conflict() at BusCompanyServiceTest

--- src/test/java/br/com/javamastery/busapp_api/service/BusCompanyServiceTest.java
+++ src/test/java/br/com/javamastery/busapp_api/service/BusCompanyServiceTest.java
@@ -1,16 +1,24 @@
 package br.com.javamastery.busapp_api.service;
 
 import br.com.javamastery.busapp_api.dto.BusCompanyRequest;
+import br.com.javamastery.busapp_api.exception.HandlerConfig;
 import br.com.javamastery.busapp_api.model.BusCompany;
 import br.com.javamastery.busapp_api.repository.BusCompanyRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowableOfType;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class BusCompanyServiceTest {
@@ -40,5 +48,13 @@ public class BusCompanyServiceTest {
         busCompany.setUpdatedAt(LocalDateTime.now());
     }
 
-    
+    @Test
+    @DisplayName("Should throw CONFLICT when email already exists")
+    void emailAlreadyExits_Conflict(){
+        when(repository.existsByEmail(busCompany.getEmail())).thenReturn(true);
+
+        HandlerConfig ex = catchThrowableOfType(() -> repository.existsByEmail(busCompany.getEmail()), HandlerConfig.class);
+
+        assertThat(ex.getStatus()).isEqualTo(HttpStatus.CONFLICT);
+    }
 }
